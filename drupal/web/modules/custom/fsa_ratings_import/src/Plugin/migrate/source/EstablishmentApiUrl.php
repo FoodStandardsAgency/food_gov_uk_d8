55f4fd399512c058f45b45b605baba0350ced2ed
Tomi Mikola <tomi.mikola@gmail.com>
2017-06-16 10:11:52 +0300

    Add 'import_mode' configuration option to ratings import.
    Add the following line into the setting.local.php for local development:
    $config['fsa_ratings_import']['import_mode'] = 'development';

--- drupal/web/modules/custom/fsa_ratings_import/src/Plugin/migrate/source/EstablishmentApiUrl.php
+++ drupal/web/modules/custom/fsa_ratings_import/src/Plugin/migrate/source/EstablishmentApiUrl.php
@@ -15,26 +15,33 @@ use Drupal\migrate\Plugin\MigrationInterface;
  */
 class EstablishmentApiUrl extends Url {
 
+  const RATINGS_API_MAX_PAGE_SIZE = 5000;
+
   /**
    * {@inheritdoc}
    */
   public function __construct(array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration) {
 
     $count = FhrsApiController::totalCount();
+    $page_count = $count / self::RATINGS_API_MAX_PAGE_SIZE;
+    $start_at_page = 1;
+    $page_size = self::RATINGS_API_MAX_PAGE_SIZE;
 
-    $pagesize = 5000;
-    $length = $count/$pagesize;
-    $calls = 0;
-
-    // @TODO: Don't try to fetch everything until module fully ready.
-    // Use the Url plugin provided config option.
-    $configuration['urls'] = [];
-    for ($i = 1; $i <= $length; $i++) {
-//      $configuration['urls'][] = $configuration['base_url'] . '?pageSize=' . $pagesize . '&pageNumber=' . $i;
+    $import_mode = \Drupal::config('fsa_ratings_import')->get('import_mode');
+    // For development we'll want to have less processing and import to happen
+    if ($import_mode == 'development') {
+      // Choose random page to be imported
+      $start_at_page = rand(100, $page_count-100);
+      // Import only one page of 100 items
+      $page_count = $start_at_page;
+      $page_size = 100;
     }
 
-    // @todo: hardcoded small batch of import
-    $configuration['urls'][] = $configuration['base_url'] . '?pageSize=10&pageNumber=50000';
+    // Use the Url plugin provided config option.
+    $configuration['urls'] = [];
+    for ($i = $start_at_page; $i <= $page_count; $i++) {
+      $configuration['urls'][] = $configuration['base_url'] . '?pageSize=' . $page_size . '&pageNumber=' . $i;
+    }
 
     // Pass in the URL's to fetch the content from.
     $this->sourceUrls = $configuration['urls'];
