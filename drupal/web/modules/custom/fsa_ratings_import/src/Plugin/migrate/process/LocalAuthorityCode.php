d9beb58e2ccac1e49bc29bc37602ebd2ba43c756
Timo Kirkkala <timo.kirkkala@wunder.io>
2017-06-22 14:55:15 +0300

    Oops, removed that var_dump() leftover

--- drupal/web/modules/custom/fsa_ratings_import/src/Plugin/migrate/process/LocalAuthorityCode.php
+++ drupal/web/modules/custom/fsa_ratings_import/src/Plugin/migrate/process/LocalAuthorityCode.php
@@ -23,7 +23,6 @@ class LocalAuthorityCode extends ProcessPluginBase {
    * {@inheritdoc}
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
-    var_dump($value);
     $value = 1 . $value;
     return $value;
   }
