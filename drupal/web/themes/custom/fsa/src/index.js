edeed210561a7f8eb607618729826355d0930574
Oskari Heinonen <oskari.heinonen@wunderkraut.com>
2017-06-26 13:23:50 +0300

    FSA-69: JavaScript to convert table to list, styles, package.json updated

--- drupal/web/themes/custom/fsa/src/index.js
+++ drupal/web/themes/custom/fsa/src/index.js
@@ -1,11 +1,17 @@
 import _ from 'lodash';
 import svg4everybody from 'svg4everybody';
+import responsiveTables from './responsiveTables';
 
 // Require every image asset inside of img folder
 require.context("./img/", true, /\.(gif|png|svg|jpe?g)$/);
 require('./css/style.css');
 
-//svg4everybody({ polyfill: true });
+// Polyfill svgs
+svg4everybody({ polyfill: true });
+
+// Responsive tables
+const tables = [...document.querySelectorAll('.js-table')];
+responsiveTables(tables);
 
 // Add class if touch device
 document.addEventListener('touchstart', function addtouchclass(e) {
---
+++ drupal/web/themes/custom/fsa/src/responsiveTables.js
@@ -0,0 +1,26 @@
+function responsiveTables(elements) {
+  const elemChildren = elements.map(elements => elements.children);
+
+  const headerTexts = elemChildren.map(function(row) {
+    const currentRow = [...row];
+    for (let i = 0; i < currentRow.length; i++) {
+      return currentRow[i].classList.contains('js-table-header') ? [...currentRow[i].children].map(function(text) {
+        return text.innerHTML;
+      }) : "";
+    }
+  });
+
+  for (let i = 0; i < elemChildren.length; i++) {
+    for (let y = 0; y < elemChildren[i].length; y++) {
+      if (!elemChildren[i][y].classList.contains('js-table-header')) {
+        const currentChildren = [...elemChildren[i][y].children];
+        for (var x = 0; x < currentChildren.length; x++) {
+          console.log(headerTexts[i][x]);
+          currentChildren[x].dataset.header = headerTexts[i][x];
+        }
+      }
+    }
+  }
+}
+
+module.exports = responsiveTables;
