21162ba06e3e418fcfbb9e0c3690d7ccb0b621ca
Onni Hakala <onni@keksi.io>
2017-07-12 18:27:02 +0300

    Enable Travis CI for stage deployments from master branch

--- drupal/drush/fsa.aliases.drushrc.php
+++ drupal/drush/fsa.aliases.drushrc.php
@@ -1,16 +1,23 @@
 <?php
-$home = drush_server_home();
-// Solve the key file to use
-$path = explode('/', dirname(__FILE__));
-array_pop($path);
-array_pop($path);
-$path[] = '.vagrant';
-$path = implode('/', $path);
-$key = shell_exec('find ' . $path . ' -iname private_key');
-if (!$key) {
-  $key = $home . '/.vagrant.d/insecure_private_key';
+// If the .vagrant folder exists find the ssh key for the virtual machine
+if (getenv('WKV_SITE_ENV' == 'local')) {
+  $home = drush_server_home();
+  // Solve the key file to use
+  $path = explode('/', dirname(__FILE__));
+  array_pop($path);
+  array_pop($path);
+  $path[] = '.vagrant';
+  $path = implode('/', $path);
+  $key = shell_exec('find ' . $path . ' -iname private_key');
+  if (!$key) {
+    $key = $home . '/.vagrant.d/insecure_private_key';
+  }
+  $key = rtrim($key);
+
+} else {
+  // .vagrant directory doesn't exist, just use empty key
+  $key = "";
 }
-$key = rtrim($key);
 
 $aliases['local'] = array(
   'parent' => '@parent',
