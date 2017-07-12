a8239c31853eb91b0542cb116ca358ea8b96c75c
Oskari Heinonen <oskari.heinonen@wunderkraut.com>
2017-07-12 11:05:59 +0300

    FSA-69: Table accessibility support

--- drupal/web/themes/custom/fsa/src/responsiveTables.js
+++ drupal/web/themes/custom/fsa/src/responsiveTables.js
@@ -1,4 +1,15 @@
-function responsiveTables(elements) {
+import checkMediaQuery from './checkMediaQuery';
+
+function responsiveTables(elements, breakpoints) {
+  var currentMediaQuery = checkMediaQuery();
+
+  if (currentMediaQuery == breakpoints.small) {
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].removeAttribute('role');
+    }
+  }
+
+
   const elemChildren = elements.map(elements => elements.children);
 
   const headerTexts = elemChildren.map(function(row) {
@@ -12,6 +23,11 @@ function responsiveTables(elements) {
 
   for (let i = 0; i < elemChildren.length; i++) {
     for (let y = 0; y < elemChildren[i].length; y++) {
+      if (currentMediaQuery == breakpoints.small) {
+        if (elemChildren[i][y].hasAttribute('role')) {
+          elemChildren[i][y].removeAttribute('role');
+        }
+      }
       if (!elemChildren[i][y].classList.contains('js-table-header')) {
         const currentChildren = [...elemChildren[i][y].children];
         for (var x = 0; x < currentChildren.length; x++) {
