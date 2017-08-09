430d29e099439a581f8c7c415bbdf26a0f87dd70
Timo Kirkkala <timo.kirkkala@wunder.io>
2017-08-09 17:00:07 +0300

    FSA-88: Initial google places autocomplete functionality attached to a form field

---
+++ drupal/web/modules/custom/fsa_establishment_lookup/js/autocomplete.js
@@ -0,0 +1,11 @@
+function initialize() {
+    var input = document.getElementById('edit-textfield'); // @todo: this is for testing, figure out how to control which field(s) should implement the autocomplete.
+    var options = {
+        componentRestrictions: {
+            country: "gb"
+        }
+    };
+    var autocomplete = new google.maps.places.Autocomplete(input, options);
+    autocomplete.setTypes(['establishment']);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
