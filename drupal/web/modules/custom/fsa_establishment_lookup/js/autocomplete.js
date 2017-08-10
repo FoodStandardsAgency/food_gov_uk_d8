4141624342523dda95d7e3bdabe650c402d8bcfd
Timo Kirkkala <timo.kirkkala@wunder.io>
2017-08-10 12:29:26 +0300

    FSA-88: Get postal code from google places result

--- drupal/web/modules/custom/fsa_establishment_lookup/js/autocomplete.js
+++ drupal/web/modules/custom/fsa_establishment_lookup/js/autocomplete.js
@@ -14,6 +14,19 @@
                 };
                 var autocomplete = new google.maps.places.Autocomplete(input, options);
                 autocomplete.setTypes(['establishment']);
+
+                // Get the postal code from selected google places entry.
+                google.maps.event.addListener(autocomplete, 'place_changed', function() {
+                    var place = autocomplete.getPlace();
+                    for (var i = 0; i < place.address_components.length; i++) {
+                        for (var j = 0; j < place.address_components[i].types.length; j++) {
+                            if (place.address_components[i].types[j] === 'postal_code') {
+                                // @todo: save this somewhere to map to an LA.
+                                console.log(place.address_components[i].long_name);
+                            }
+                        }
+                    }
+                })
             }
             google.maps.event.addDomListener(window, 'load', init_places_autocomplete);
 
@@ -21,4 +34,3 @@
     };
 
 })(jQuery, Drupal, drupalSettings);
-
