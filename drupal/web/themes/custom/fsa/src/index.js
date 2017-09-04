d49880b5e0a63ff6a3e0a040995d15650eb1e512
Oskari Heinonen <oskari.heinonen@wunderkraut.com>
2017-09-04 11:30:17 +0300

    FSA-171: Toc navigation scrolling

--- drupal/web/themes/custom/fsa/src/index.js
+++ drupal/web/themes/custom/fsa/src/index.js
@@ -64,10 +64,10 @@ if (toggleButtons != null) {
 }
 
 // Toggle content
-const tableOfContents = document.querySelectorAll('.toc');
+const tableOfContentsElement = document.querySelectorAll('.toc-tree');
 const contentElement = document.querySelectorAll('.toc-filter');
-if (tableOfContents != null || contentElement != null) {
-  toc(tableOfContents, contentElement);
+if (tableOfContentsElement != null || contentElement != null) {
+  toc(tableOfContentsElement, contentElement);
 }
 
 // Mobile menu
