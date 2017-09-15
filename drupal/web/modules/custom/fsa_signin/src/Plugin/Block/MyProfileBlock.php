dc7303171d4347f4182fb3126ca46794e0966529
Tomi Mikola <tomi.mikola@gmail.com>
2017-09-15 14:06:38 +0300

    Show different links on the My profile block for auth/anon users.

--- drupal/web/modules/custom/fsa_signin/src/Plugin/Block/MyProfileBlock.php
+++ drupal/web/modules/custom/fsa_signin/src/Plugin/Block/MyProfileBlock.php
@@ -22,23 +22,27 @@ class MyProfileBlock extends BlockBase {
   public function build() {
     $build = [];
 
+    // Disable block cache
+    $build['#cache'] = ['max-age' => 0];
+
     $markup  = '<ul>';
-    $markup .= self::createLinkMarkup('fsa_signin.default_controller_emailSubscriptionsPage', 'Email subscriptions', '<li>', '</li>');
-    $markup .= self::createLinkMarkup('fsa_signin.default_controller_smsSubscriptionsPage', 'SMS subscriptions', '<li>', '</li>');
-    $markup .= self::createLinkMarkup('fsa_signin.default_controller_myAccountPage', 'My account', '<li>', '</li>');
-    $markup .= self::createLinkMarkup('user.logout.http', 'Logout', '<li>', '</li>');
+    if (\Drupal::currentUser()->isAuthenticated()) {
+      $markup .= self::createLinkMarkup('fsa_signin.default_controller_emailSubscriptionsPage', 'Email subscriptions', '<li>', '</li>');
+      $markup .= self::createLinkMarkup('fsa_signin.default_controller_smsSubscriptionsPage', 'SMS subscriptions', '<li>', '</li>');
+      $markup .= self::createLinkMarkup('fsa_signin.default_controller_myAccountPage', 'My account', '<li>', '</li>');
+      $markup .= self::createLinkMarkup('user.logout.http', 'Logout', '<li>', '</li>');
+    }
+    else {
+      $markup .= self::createLinkMarkup('user.login.http', 'Sign in', '<li>', '</li>');
+    }
     $markup .= '</ul>';
 
     $build['my_profile_block']['#markup'] = $markup;
-
     return $build;
   }
 
   protected static function createLinkMarkup($route_name, $text, $prefix = '', $suffix = '') {
-    $url = Url::fromRoute($route_name);
-    $link = Link::fromTextAndUrl(t($text), $url);
-    $link = $link->toRenderable();
-    $link['#attributes'] = ['class' => ['profile-link']];
-    return $prefix . render($link) . $suffix;
+    $link_object = Link::createFromRoute($text, $route_name);
+    return $prefix . $link_object->toString() . $suffix;
   }
 }
