404e5d808d1466914b595f59744f6beae7fc7be4
Tomi Mikola <tomi.mikola@gmail.com>
2017-09-15 15:13:02 +0300

    Set 'active' highlighting for the profile menu

--- drupal/web/modules/custom/fsa_signin/src/Plugin/Block/MyProfileBlock.php
+++ drupal/web/modules/custom/fsa_signin/src/Plugin/Block/MyProfileBlock.php
@@ -25,7 +25,7 @@ class MyProfileBlock extends BlockBase {
     // Disable block cache
     $build['#cache'] = ['max-age' => 0];
 
-    $markup  = '<ul>';
+    $markup  = '<div class="navigation"><ul>';
     if (\Drupal::currentUser()->isAuthenticated()) {
       $markup .= self::createLinkMarkup('fsa_signin.default_controller_emailSubscriptionsPage', 'Email subscriptions', '<li>', '</li>');
       $markup .= self::createLinkMarkup('fsa_signin.default_controller_smsSubscriptionsPage', 'SMS subscriptions', '<li>', '</li>');
@@ -35,14 +35,18 @@ class MyProfileBlock extends BlockBase {
     else {
       $markup .= self::createLinkMarkup('user.login.http', 'Sign in', '<li>', '</li>');
     }
-    $markup .= '</ul>';
+    $markup .= '</ul></div>';
 
     $build['my_profile_block']['#markup'] = $markup;
     return $build;
   }
 
   protected static function createLinkMarkup($route_name, $text, $prefix = '', $suffix = '') {
-    $link_object = Link::createFromRoute($text, $route_name);
+    $options = [];
+    if (\Drupal::routeMatch()->getRouteName() == $route_name) {
+      $options['attributes'] = ['class' => 'is-active'];
+    }
+    $link_object = Link::createFromRoute($text, $route_name, [], $options);
     return $prefix . $link_object->toString() . $suffix;
   }
 }
