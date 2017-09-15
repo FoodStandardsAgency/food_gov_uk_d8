498f0fe4b6fe20cd6c4b544342dda8ef75b84b01
Timo Kirkkala <timo.kirkkala@wunder.io>
2017-09-15 16:05:33 +0300

    Backlink to alert page sidebar_first

---
+++ drupal/web/modules/custom/fsa_alerts/src/Plugin/Block/AlertBacklink.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\fsa_alerts\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Link;
+
+/**
+ * Provides a 'AlertBacklink' block.
+ *
+ * @Block(
+ *  id = "alert_backlink",
+ *  admin_label = @Translation("Alert page backlink"),
+ * )
+ */
+class AlertBacklink extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+
+    $options = ['attributes' => ['class' => 'arrow-back']];
+
+    // Link to News & Alerts listing page.
+    $build['backlink'] = [
+      '#markup' => Link::createFromRoute(t('Back'), 'entity.node.canonical', ['node' => '56'], $options)->toString(),
+    ];
+
+    return $build;
+  }
+
+}
