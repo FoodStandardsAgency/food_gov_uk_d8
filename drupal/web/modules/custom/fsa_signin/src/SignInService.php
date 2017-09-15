508bb140de52306ae6bcea4be176622d69d92de4
Tomi Mikola <tomi.mikola@gmail.com>
2017-09-15 12:11:58 +0300

    Service with helper methods.

---
+++ drupal/web/modules/custom/fsa_signin/src/SignInService.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\fsa_signin;
+
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\user\Entity\User;
+
+/**
+ * Class SignInService.
+ */
+class SignInService {
+
+  use StringTranslationTrait;
+
+  /**
+   * Constructs a new DefaultService object.
+   */
+  public function __construct() {
+
+  }
+
+
+  /**
+   * @return array
+   */
+  public function allergenTermsAsOptions() {
+    $all_terms = \Drupal::entityTypeManager()
+      ->getStorage('taxonomy_term')
+      ->loadTree('alerts_allergen', 0, 1, FALSE);
+    $options = [];
+    foreach ($all_terms as $term) {
+      $options[$term->tid] = $this->t($term->name);
+    }
+    return $options;
+  }
+
+  /**
+   * @param \Drupal\user\Entity\User $account
+   * @return int[] Term IDs
+   */
+  public function subscribedTermIds(User $account) {
+    $subscriptions = $account->get('field_subscribed_notifications')
+      ->getValue();
+    $subscribed_term_ids = [];
+    foreach ($subscriptions as $s) {
+      $subscribed_term_ids[] = intval($s['target_id']);
+    }
+    return $subscribed_term_ids;
+  }
+}
