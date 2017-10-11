90e4565b7793a5df657a69911417e2a4e94354ce
24ma13wg <24ma13wg@gmail.com>
2017-10-11 16:44:30 +0100

    FSA-9: Update find food safety team form module

--- drupal/web/modules/custom/fsa_team_finder/src/Controller/FsaTeamFinder.php
+++ drupal/web/modules/custom/fsa_team_finder/src/Controller/FsaTeamFinder.php
@@ -4,6 +4,8 @@ namespace Drupal\fsa_team_finder\Controller;
 
 class FsaTeamFinder {
   public function render() {
-    return \Drupal::formBuilder()->getForm('Drupal\fsa_team_finder\Form\TeamFinder');
+    $output[] = array('#markup' => t('<p>Food safety teams advise consumers and businesses on food related issues, such as inspections of food premises, and concerns and complaints raised by the public.</p>'));
+    $output[] = \Drupal::formBuilder()->getForm('Drupal\fsa_team_finder\Form\TeamFinder');
+    return $output;
   }
 }
--- drupal/web/modules/custom/fsa_team_finder/src/Form/TeamFinder.php
+++ drupal/web/modules/custom/fsa_team_finder/src/Form/TeamFinder.php
@@ -4,6 +4,7 @@ namespace Drupal\fsa_team_finder\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
 use Drupal\Component\Serialization\Json;
 use GuzzleHttp\Exception\RequestException;
 
@@ -24,15 +25,15 @@ class TeamFinder extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form['about'] = array(
-      '#type' => 'item',
-      '#markup' => t('Food safety teams advise consumers and businesses on food related issues, such as inspections of food premises, and concerns and complaints raised by the public.'),
-    );
+
+
+    $form['#prefix'] = '<div id="fsa-team-finder-wrapper">';
+    $form['#suffix'] = '</div>';
     $form['progress'] = array(
       '#type' => 'item',
       '#markup' => t('Step 1 of 2'),
     );
-    $form['postcode'] = array(
+    $form['query'] = array(
       '#type' => 'textfield',
       '#title' => t('Find a food safety team'),
       '#description' => t('<div>Please enter a postcode to find the food safety team in the area.</div><div>Enter a full postcode</div>'),
@@ -41,35 +42,73 @@ class TeamFinder extends FormBase {
       '#maxlength' => 9,
       '#required' => TRUE,
     );
-    $form['actions']['submit'] = array(
-      '#type' => 'submit',
-      '#value' => $this->t('Submit'),
+    $form['actions'] = array(
+      '#type' => 'actions',
+      'submit' => array(
+        '#type' => 'submit',
+        '#value' => $this->t('Submit'),
+        '#ajax' => array(
+          'callback' => '::rebuildForm',
+          'event' => 'click',
+          'wrapper' => 'fsa-team-finder-wrapper',
+        ),
+      ),
     );
     return $form;
   }
 
   /**
-   * {@inheritdoc}
+   * Rebuilds form with query result.
+   *
+   * @param array $form
+   * @param $form_state
+   *
+   * @return array $form
    */
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-    if (strlen($form_state->getValue('postcode')) < 6) {
-      $form_state->setErrorByName('postcode', $this->t('Error!'));
-    }
+  public function rebuildForm(array $form, FormStateInterface $form_state) {
+    $form['progress']['#markup'] = t('Step 2 of 2');
+    unset($form['query']);
+    unset($form['actions']);
+    $form['confirmation'] = array(
+      '#type' => 'item',
+      '#markup' => $this->t('<h2>Food safety team details</h2><p>Details of the food safety team covering <strong>@query</strong> are shown below. Please contact them to report the food problem.</p>', array(
+        '@query' => $form_state->getValue('query'),
+      )),
+    );
+    $la = $this->getLocalAuthority($form_state->getValue('query'));
+    $email = '';
+    $website = '';
+    $form['details'] = array(
+      '#type' => 'item',
+      '#markup' => t('<p><strong>@name</strong><br />Email address: ' . $email . '<br />Website: ' . $website . '</p>', array(
+        '@name' => $la['name'],
+      )),
+    );
+    $form['back'] = array(
+      '#title' => $this->t('Back to form'),
+      '#type' => 'link',
+      '#url' => Url::fromRoute('fsa_team_finder.render')
+    );
+    return $form;
   }
 
   /**
-   * {@inheritdoc}
+   * Provides local authority ONS code and name.
+   *
+   * @param string
+   *
+   * @return array
    */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
+  public function getLocalAuthority($query) {
 
     // build mapit request
-    /*$base = 'https://mapit.mysociety.org';
-    $postcode = str_replace(' ', '', 'E16 1BF');
+    $base = 'https://mapit.mysociety.org';
+    $postcode = str_replace(' ', '', $query);
     $key = 'KEY';
     $url = $base . '/postcode/' . $postcode . '?api_key=' . $key;
 
     // call mapit
-    $client = \Drupal::httpClient();
+    /*$client = \Drupal::httpClient();
     $client->request('GET', $url);
     try {
       $response = $client->get($url);
@@ -77,11 +116,41 @@ class TeamFinder extends FormBase {
     }
     catch (RequestException $e) {
       watchdog_exception('fsa_team_finder', $e->getMessage());
-    }*/
+    }
+    $council = $data['shortcuts']['council'];
+    return array(
+      'ons' => $data['areas'][$council]['codes']['ons'],
+      'name' => $data['areas'][$council]['name'],
+    );*/
 
-    // tests
-    /*drupal_set_message(t('Postcode: @result', array('@result' => var_export($data['postcode'], TRUE))));
-    drupal_set_message(t('Name: @result', array('@result' => var_export($data['areas']['2510']['name'], TRUE))));*/
+    // save on mapit api calls
+    return array(
+      'ons' => '00BG',
+      'name' => 'Tower Hamlets Borough Council',
+    );
   }
+
+  /**
+   * Maps local authority ONS code to identifier.
+   *
+   * @param string
+   *
+   * @return integer
+   */
+  public function MapOnsToId($ons) {
+    // create a mappings table in an install file
+    // query the id
+    return $id;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {}
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {}
 
 }
