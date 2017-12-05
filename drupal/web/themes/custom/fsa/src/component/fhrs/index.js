71743c376eedce578dc0b0224b5bc812fe8f199d
Oskari Heinonen <oskari.heinonen@wunderkraut.com>
2017-12-05 12:27:10 +0200

    FSA-415: FHRS bug fixing: mobile styles

---
+++ drupal/web/themes/custom/fsa/src/component/fhrs/index.js
@@ -0,0 +1,14 @@
+// Import HTML
+import html from './fhrs.html';
+
+// Import Styles
+import css from './fhrs.css';
+
+// Import JavaScript
+import js from './fhrs';
+
+module.exports = {
+  html,
+  css,
+  js,
+};
