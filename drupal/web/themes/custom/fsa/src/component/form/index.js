0f2e41f51a99c6ab38f2dfe9ce311044dcd767ef
Oskari Heinonen <oskari.heinonen@wunderkraut.com>
2017-12-05 10:00:11 +0200

    FSA-378: Added new npm package, cleaned the theme folder

---
+++ drupal/web/themes/custom/fsa/src/component/form/index.js
@@ -0,0 +1,14 @@
+// Import HTML
+import html from './form.html';
+
+// Import Styles
+import css from './form.css';
+
+// Import JavaScript
+import js from './form';
+
+module.exports = {
+  html,
+  css,
+  js,
+};
