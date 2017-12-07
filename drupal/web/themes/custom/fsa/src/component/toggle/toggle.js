67e6c1ab4c8f72b33b0a5ba46f6d048f323a7184
Oskari Heinonen <oskari.heinonen@wunderkraut.com>
2017-12-07 15:23:38 +0200

    FSA-415: Toggle content temp hidden

--- drupal/web/themes/custom/fsa/src/component/toggle/toggle.js
+++ drupal/web/themes/custom/fsa/src/component/toggle/toggle.js
@@ -1,6 +1,6 @@
 import checkMediaQuery from '../../core/helper/checkMediaQuery';
 
-function toggle(toggleButtons, breakpoints) {
+function toggle() {
 
   // const toggleContentVisibility = (button, content) => {
   //   if (content.classList.contains('is-open')) {
@@ -54,50 +54,83 @@ function toggle(toggleButtons, breakpoints) {
   //     toggleContentVisibility(toggleButtons, content);
   //   });
   // }
+
   // Measure all content elements and assign their height to a css variable in the style attribute of the html.
-function measureAccordionContents(  ) {
-  document.querySelectorAll('.js-accordion-content')
-    .forEach( function( element, index ) {
-    var content = element.querySelector(':first-child');
-    element.style.setProperty('--expanded' , content.offsetHeight + 'px');
-  } );
-}
-
-measureAccordionContents();
-
-window.addEventListener('resize', measureAccordionContents);
-/*
-@todo - Needs to be rerun whenever 
-        dom changes affect the width
-        of the parent or the height of 
-        the children.
-@todo - At narrow viewport widths if
-        there is no scrollbar intially 
-        the content height is measuerd 
-        for that viewport.
-        Then when the content is shown 
-        and it is long enough to trigger 
-        a scrollbar, the initially measured 
-        height is no longer adequate.
-*/
-  document.querySelectorAll('.js-toggle-content')
-    .forEach( (e) => e.addEventListener("transitionend", measureAccordionContents) );
-
-
-
-  function accordionEventHandler( clickEvent )
-  {
-    clickEvent.preventDefault();
-    measureAccordionContents();
-    var clickedItem = clickEvent.target;
-    var content = clickedItem.nextElementSibling;
-    console.log(clickedItem.nextElementSibling);
-    content.classList.toggle('is-open');
+  function measureAccordionContents() {
+    document.querySelectorAll('.js-toggle-content')
+      .forEach(( element, index ) => {
+        console.log(element.querySelector(':first-child'));
+        // var content = element.querySelector(':first-child');
+        element.style.setProperty('--expanded' , element.offsetHeight + 'px');
+      });
   }
 
-  document.querySelectorAll('.js-toggle-button')
-    .forEach( (e,i) => e.addEventListener('click', accordionEventHandler) );
+  measureAccordionContents();
 
+  window.addEventListener('resize', measureAccordionContents);
+  /*
+  @todo - Needs to be rerun whenever 
+          dom changes affect the width
+          of the parent or the height of 
+          the children.
+  @todo - At narrow viewport widths if
+          there is no scrollbar intially 
+          the content height is measuerd 
+          for that viewport.
+          Then when the content is shown 
+          and it is long enough to trigger 
+          a scrollbar, the initially measured 
+          height is no longer adequate.
+  */
+
+  // function accordionEventHandler(button, content) {
+  //   if (content.classList.contains('is-open')) {
+  //     content.classList.remove('is-open');
+  //     content.classList.add('is-closed');
+  //     content.setAttribute('aria-hidden', true);
+  //     button.classList.remove('is-open');
+  //     button.setAttribute('aria-expanded', false);
+  //   } else {
+  //     content.classList.add('is-open');
+  //     content.classList.remove('is-closed');
+  //     content.setAttribute('aria-hidden', false);
+  //     button.classList.add('is-open');
+  //     button.setAttribute('aria-expanded', true);
+  //   }
+  // }
+
+  // // All the toggle buttons
+  // const toggleButtonElementArray = [...document.querySelectorAll('.js-toggle-button')];
+
+  // // Check everything found
+  // if (toggleButtonElementArray <= 0) {
+  //   return;
+  // }
+
+  // // Loop
+  // toggleButtonElementArray.forEach((element) => {
+  //   const toggleButtonElement = element;
+  //   let toggleButton;
+  //   let content;
+
+  //   if (toggleButtonElement.nextElementSibling === null) {
+  //     content = toggleButtonElement.closest('.js-content-next').nextElementSibling;
+  //     toggleButton = toggleButtonElement.closest('.js-content-next');
+  //   } else {
+  //     content = toggleButtonElement.nextElementSibling;
+  //     toggleButton = toggleButtonElement;
+  //   }
+    
+  //   // Add click listener to toggle
+  //   toggleButtonElement.addEventListener("click", function(e){
+  //     e.preventDefault();
+  //     accordionEventHandler(toggleButton, content);
+  //   });
+
+  //   // Add transitioned listener to content
+  //   content.addEventListener("transitionend", measureAccordionContents);
+  // });
+    
 }
 
 module.exports = toggle;
