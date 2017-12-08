80ac4fa9d09ce966fedc18a3475df74edb06c604
Oskari Heinonen <oskari.heinonen@wunderkraut.com>
2017-12-08 11:02:34 +0200

    FSA-415: New toggle functionlality wip

--- drupal/web/themes/custom/fsa/src/component/toggle/toggle.js
+++ drupal/web/themes/custom/fsa/src/component/toggle/toggle.js
@@ -1,4 +1,5 @@
 import checkMediaQuery from '../../core/helper/checkMediaQuery';
+import cssCustomPropertySupport from '../../core/helper/cssCustomPropertySupport';
 
 function toggle() {
 
@@ -56,18 +57,13 @@ function toggle() {
   // }
 
   // Measure all content elements and assign their height to a css variable in the style attribute of the html.
-  function measureAccordionContents() {
-    document.querySelectorAll('.js-toggle-content')
-      .forEach(( element, index ) => {
-        console.log(element.querySelector(':first-child'));
-        // var content = element.querySelector(':first-child');
-        element.style.setProperty('--expanded' , element.offsetHeight + 'px');
-      });
+  function measureAccordionContents(element) {
+    let childrenCombinedHeight = 0;
+    [...element.children].forEach((child) => {
+      childrenCombinedHeight = childrenCombinedHeight + child.offsetHeight;
+    });
+    element.style.setProperty('--expanded' , `${childrenCombinedHeight}px`);
   }
-
-  measureAccordionContents();
-
-  window.addEventListener('resize', measureAccordionContents);
   /*
   @todo - Needs to be rerun whenever 
           dom changes affect the width
@@ -83,53 +79,58 @@ function toggle() {
           height is no longer adequate.
   */
 
-  // function accordionEventHandler(button, content) {
-  //   if (content.classList.contains('is-open')) {
-  //     content.classList.remove('is-open');
-  //     content.classList.add('is-closed');
-  //     content.setAttribute('aria-hidden', true);
-  //     button.classList.remove('is-open');
-  //     button.setAttribute('aria-expanded', false);
-  //   } else {
-  //     content.classList.add('is-open');
-  //     content.classList.remove('is-closed');
-  //     content.setAttribute('aria-hidden', false);
-  //     button.classList.add('is-open');
-  //     button.setAttribute('aria-expanded', true);
-  //   }
-  // }
+  function accordionEventHandler(button, content) {
+    if (content.classList.contains('is-open')) {
+      content.classList.remove('is-open');
+      content.classList.add('is-closed');
+      content.setAttribute('aria-hidden', true);
+      button.classList.remove('is-open');
+      button.setAttribute('aria-expanded', false);
+    } else {
+      content.classList.add('is-open');
+      content.classList.remove('is-closed');
+      content.setAttribute('aria-hidden', false);
+      button.classList.add('is-open');
+      button.setAttribute('aria-expanded', true);
+    }
+  }
 
-  // // All the toggle buttons
-  // const toggleButtonElementArray = [...document.querySelectorAll('.js-toggle-button')];
+  // All the toggle buttons
+  const toggleButtonElementArray = [...document.querySelectorAll('.js-toggle-button')];
 
-  // // Check everything found
-  // if (toggleButtonElementArray <= 0) {
-  //   return;
-  // }
+  // Check everything found
+  if (toggleButtonElementArray <= 0) {
+    return;
+  }
 
-  // // Loop
-  // toggleButtonElementArray.forEach((element) => {
-  //   const toggleButtonElement = element;
-  //   let toggleButton;
-  //   let content;
+  // Loop
+  toggleButtonElementArray.forEach((element) => {
+    const toggleButtonElement = element;
+    let toggleButton;
+    let content;
 
-  //   if (toggleButtonElement.nextElementSibling === null) {
-  //     content = toggleButtonElement.closest('.js-content-next').nextElementSibling;
-  //     toggleButton = toggleButtonElement.closest('.js-content-next');
-  //   } else {
-  //     content = toggleButtonElement.nextElementSibling;
-  //     toggleButton = toggleButtonElement;
-  //   }
+    if (toggleButtonElement.nextElementSibling === null) {
+      content = toggleButtonElement.closest('.js-content-next').nextElementSibling;
+      toggleButton = toggleButtonElement.closest('.js-content-next');
+    } else {
+      content = toggleButtonElement.nextElementSibling;
+      toggleButton = toggleButtonElement;
+    }
+
+    // Initial measurmenet for content element
+    measureAccordionContents(content);
     
-  //   // Add click listener to toggle
-  //   toggleButtonElement.addEventListener("click", function(e){
-  //     e.preventDefault();
-  //     accordionEventHandler(toggleButton, content);
-  //   });
+    // Add click listener to toggle
+    toggleButtonElement.addEventListener("click", function(e){
+      e.preventDefault();
+      accordionEventHandler(toggleButton, content);
+    });
 
-  //   // Add transitioned listener to content
-  //   content.addEventListener("transitionend", measureAccordionContents);
-  // });
+    // Add transitioned listener to content
+    content.addEventListener("transitionend", function(e){
+      measureAccordionContents(content);
+    });
+  });
     
 }
 
