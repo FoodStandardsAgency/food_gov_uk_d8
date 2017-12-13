cc21c3582b35428995f3adacfd5760f0d09d12a7
Oskari Heinonen <oskari.heinonen@wunderkraut.com>
2017-12-14 00:19:29 +0200

    FSA-451: Toggle functionality

--- drupal/web/themes/custom/fsa/src/component/toggle/toggle.js
+++ drupal/web/themes/custom/fsa/src/component/toggle/toggle.js
@@ -3,8 +3,14 @@ import breakpoints from '../../core/helper/breakpoints';
 import cssCustomPropertySupport from '../../core/helper/cssCustomPropertySupport';
 import debounce from '../../core/helper/debounce';
 import nextByClass from '../../core/helper/nextByClass';
+import inert from 'wicg-inert';
 
 function toggle() {
+
+  const KEYCODE = {
+    ESC: 27,
+    SPACE: 32,
+  }
 
   // Measure all content elements and assign their height to a css variable in the style attribute of the html.
   function setDynamicHeight(element) {
@@ -53,15 +59,30 @@ function toggle() {
     element.classList.remove('is-visible');
     element.classList.add('is-hidden');
     element.setAttribute('aria-hidden', true);
+
+    // Unset content from accessibility tree
+    element.inert = true;
   }
 
   function accordionContentOpen(element) {
     element.classList.add('is-visible');
     element.classList.remove('is-hidden');
     element.setAttribute('aria-hidden', false);
+
+    // Set content to accessibility tree
+    element.inert = false;
   }
 
-  function accordionEventHandler(button, content) {
+  function accordionEventHandler(button, content, event) {
+    // console.log(event.type  + ' ' + content.classList.contains('is-visible'));
+    // if (document.activeElement.classList.contains('js-toggle-button')) {
+    //   console.log(button === document.activeElement)
+    // } else {
+    //   console.log(button);
+    //   console.log(document.activeElement.closest('.js-toggle-button'));
+    //   console.log(button === document.activeElement.closest('.js-toggle-button'));
+    // }
+    
     if (content.classList.contains('is-visible')) {
       accordionContentClose(content);
       accordionButtonClose(button);
@@ -86,6 +107,9 @@ function toggle() {
     });
 
     contentArray.forEach((element) => {
+      // Unset content from accessibility tree
+      element.inert = true;
+
       // Check if only mobile
       if (element.classList.contains('js-toggle-content-only-mobile')) {
         if (checkMediaQuery() == breakpoints.small) {
@@ -117,26 +141,61 @@ function toggle() {
   toggleButtonElementArray.forEach((element) => {
     let toggleButton = element;
     let content = nextByClass(toggleButton, 'js-toggle-content');
+    let theme = undefined;
 
     if (content === null) {
       content = toggleButton.closest('.js-content-next').nextElementSibling;
       toggleButton = element.closest('.js-content-next');
     }
 
-    contentElementArray = [...contentElementArray, content];
-    
-    // Add click listener to toggle
-    toggleButton.addEventListener('click', function(e){
-      e.preventDefault();
-      accordionEventHandler(toggleButton, content);
+    contentElementArray = [...contentElementArray, content];    
+
+    // Check if content has a theme
+    const contentClassListArray = content.classList.value.split(' ');
+
+    contentClassListArray.forEach((className) => {
+      if (className.indexOf('js-toggle-theme') !== -1) {
+        theme = className.split('js-toggle-theme-').pop();
+        content.classList.add(`is-${theme}`);
+      }
     });
 
-    toggleButton.addEventListener('mouseenter', function(e){
+    // Add click listener to toggle
+    toggleButton.addEventListener('mousedown', function(event){
+      event.preventDefault();  //stops default browser action (focus)
+      accordionEventHandler(toggleButton, content, event);
+    });
+
+    // Add click listener to toggle
+    toggleButton.addEventListener('click', function(event){
+      event.preventDefault();
+      console.log(document.activeElement);
+    });
+
+    toggleButton.addEventListener('focus', function(event) {
+      content.style.willChange = 'max-height, min-height';
+      accordionEventHandler(toggleButton, content, event);
+    }, true);
+
+    // toggleButton.addEventListener('blur', function(event) {
+  //   content.style.willChange = 'auto';
+    //   accordionEventHandler(toggleButton, content, event);
+    // }, true);
+
+    toggleButton.addEventListener('mouseenter', function(event){
       content.style.willChange = 'max-height, min-height';
     });
 
-    toggleButton.addEventListener('mouseout', function(e){
+    toggleButton.addEventListener('mouseout', function(event){
       content.style.willChange = 'auto';
+    });
+
+    // Add click listener to toggle
+    toggleButton.addEventListener('keydown', function(event){
+      if (event.keyCode === KEYCODE.SPACE) {
+        event.preventDefault();
+        accordionEventHandler(toggleButton, content, event);
+      }
     });
     
     // // Add transitioned listener to content
