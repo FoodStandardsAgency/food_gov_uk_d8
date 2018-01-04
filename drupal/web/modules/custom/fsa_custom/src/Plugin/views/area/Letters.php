59055ba02132535dcde833906a8f6474003194a1
Timo Kirkkala <timo.kirkkala@wunder.io>
2018-01-04 15:47:09 +0200

    FSA-478: Render letters navigation as themed item_list

--- drupal/web/modules/custom/fsa_custom/src/Plugin/views/area/Letters.php
+++ drupal/web/modules/custom/fsa_custom/src/Plugin/views/area/Letters.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\fsa_custom\Plugin\views\area;
 
+use Drupal\Core\Link;
 use Drupal\views\Plugin\views\area\AreaPluginBase;
 use \Drupal\Core\Url;
 
@@ -31,29 +32,78 @@ class Letters extends AreaPluginBase {
       sort($chars);
 
       // Generate markup.
-      $output = '';
+      $items = [];
       $alphabet = range('A', 'Z');
       $view_path = Url::fromRoute('view.a_to_z.page_1')->toString();
 
       // Append any non-letters.
       foreach ($chars as $char) {
         if (!in_array($char, $alphabet)) {
-          $output .= '<a href="' . $view_path . '/' . $char . '">' . $char . '</a>';
+          $item = Link::fromTextAndUrl($char, Url::fromUserInput($view_path . '/' . strtolower($char)))->toString();
+          $items[] = [
+            '#markup' => $item,
+            '#wrapper_attributes' => [
+              'class' => [
+                'letter char',
+              ],
+            ],
+          ];
         }
       }
 
       // Append letters.
       foreach ($alphabet as $letter) {
+        $active = FALSE;
         if (in_array($letter, $chars)) {
-          $output .= '<a href="' . $view_path . '/' . strtolower($letter) . '">' . $letter . '</a>';
+          $item = Link::fromTextAndUrl($letter, Url::fromUserInput($view_path . '/' . strtolower($letter)))->toString();
+          $empty = FALSE;
+
+          // Figure out if the link is active.
+          $path_basename = basename($_SERVER['REQUEST_URI']);
+          if ($path_basename == strtolower($letter) || '/' . $path_basename == $view_path) {
+            $active = 'is-active';
+          }
+
         }
         else {
-          $output .= '<span>' . $letter . '</span>';
+          // Do not create as link if no results.
+          $item = '<span>' . $letter . '</span>';
+          $empty = 'no-results';
         }
+
+        $items[] = [
+          '#markup' => $item,
+          '#wrapper_attributes' => [
+            'class' => [
+              'letter',
+              $active,
+              $empty,
+            ],
+          ],
+        ];
+
       }
-      return ['#markup' => $output];
+
+      // Return the "menu" list.
+      return [
+        '#theme' => 'item_list',
+        '#list_type' => 'ul',
+        '#attributes' => [
+          'class' => [
+            'inline',
+            'menu-letters',
+          ],
+        ],
+        '#items' => $items,
+      ];
+
     }
+
     return [];
   }
 
 }
+
+
+
+
