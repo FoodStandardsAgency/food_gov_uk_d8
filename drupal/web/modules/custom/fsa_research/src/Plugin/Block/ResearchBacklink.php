a9b6b7f06c02e97bfbfb2717730d127e4e64e3bc
Timo Kirkkala <timo.kirkkala@wunder.io>
2018-01-11 15:49:37 +0200

    FSA-509: Use referrer path for backlink (if referring page is internal)

--- drupal/web/modules/custom/fsa_research/src/Plugin/Block/ResearchBacklink.php
+++ drupal/web/modules/custom/fsa_research/src/Plugin/Block/ResearchBacklink.php
@@ -7,6 +7,7 @@ use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Link;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Url;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Provides a 'ResearchBacklink' block.
@@ -20,33 +21,45 @@ class ResearchBacklink extends BlockBase {
 
   // Content types to show the backlink.
   const CONTENT_TYPES_TO_ENABLE = [
-    'research',
+    'research_project',
   ];
 
   /**
    * {@inheritdoc}
    */
   public function build() {
+    global $base_url;
     $build = [];
     $node = \Drupal::routeMatch()->getParameter('node');
-
-    // Set target for backlink.
-    // @todo: Logic for the backlink target path.
-    $path = '#';
+    $text = '';
+    $path = FALSE;
     if (is_object($node) && in_array($node->getType(), self::CONTENT_TYPES_TO_ENABLE)) {
       // Link research nodes back to search.
       // @todo: Get path from route.
-      $path = '/search/news-alerts/research';
+      $path = '/search/research';
+      $text = $this->t('Back to search');
+    }
+    else {
+      // Get backlink from referrer path if it's internal page referring.
+      $previousUrl = \Drupal::request()->server->get('HTTP_REFERER');
+      $parsedUrl = parse_url($previousUrl);
+
+      if (isset($parsedUrl['scheme']) && $base_url == ($parsedUrl['scheme'] . '://' . $parsedUrl['host'])) {
+        $path = $parsedUrl['path'];
+        $text = $this->t('Back');
+      }
     }
 
-    $options = ['attributes' => ['class' => 'back']];
-    $url = Url::fromUserInput($path, $options);
+    if ($path) {
+      $options = ['attributes' => ['class' => 'back']];
+      $url = Url::fromUserInput($path, $options);
 
-    // Link to News & Alerts listing page.
-    $build['backlink'] = [
-      '#markup' => Link::fromTextAndUrl($this->t('Back'), $url)
-        ->toString() . '<br /><pre>[link target not defined]</pre>',
-    ];
+      // Link to News & Alerts listing page.
+      $build['backlink'] = [
+        '#markup' => Link::fromTextAndUrl($text, $url)
+          ->toString(),
+      ];
+    }
 
     return $build;
   }
