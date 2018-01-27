cd63e9aa3779b83cbfe397e7439b9ab2b921384b
Timo Kirkkala <timo.kirkkala@wunder.io>
2018-01-27 22:21:15 +0200

    FSA-717: Refactor the custom error page module in order to display themed html/page markup with navigation and to allow theming as per design requirements. Consider 401 also a valid error case to dieplsy

---
+++ drupal/web/modules/custom/fsa_40x/src/Controller/FsaHttp4xxController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Drupal\fsa_40x\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Url;
+
+/**
+ * Controller to override Drupal default HTTP 4xx responses.
+ *
+ * @package Drupal\fsa_40x\Controller
+ */
+class FsaHttp4xxController extends ControllerBase {
+
+  /**
+   * The 404 page content override.
+   *
+   * @return array
+   *   Render array to display themed error page content.
+   */
+  public function on401() {
+    return [
+      '#theme' => 'fsa_40x_response',
+      '#status_code' => '401',
+      '#error' => $this->t('Unauthorized'),
+      '#content' => $this->t('You are not allowed to this resource. Please check the URL or go to the <a href="@home">homepage</a>.', ['@home' => self::getHomeUrl()]),
+    ];
+  }
+
+  /**
+   * The 404 page content override.
+   *
+   * @return array
+   *   Render array to display themed error page content.
+   */
+  public function on403() {
+    return [
+      '#theme' => 'fsa_40x_response',
+      '#status_code' => '403',
+      '#error' => $this->t('Access denied'),
+      '#content' => $this->t('Access has been denied. Please check the URL or go to the <a href="@home">homepage</a>.', ['@home' => self::getHomeUrl()]),
+    ];
+  }
+
+  /**
+   * The 404 page content override.
+   *
+   * @return array
+   *   Render array to display themed error page content.
+   */
+  public function on404() {
+    return [
+      '#theme' => 'fsa_40x_response',
+      '#status_code' => '404',
+      '#error' => $this->t('Page not found'),
+      '#content' => $this->t('The page you were looking for has been moved or does not exist. Please go to the <a href="@home">homepage</a> or use our search to find the information you need.', ['@home' => self::getHomeUrl()]),
+    ];
+  }
+
+  /**
+   * Helper to return correct homepage url for any language.
+   *
+   * @return \Drupal\Core\GeneratedUrl|string
+   *   Url of homepage.
+   */
+  private function getHomeUrl() {
+    return Url::fromRoute('<front>', [], ['absolute' => TRUE])->toString();
+  }
+
+}
