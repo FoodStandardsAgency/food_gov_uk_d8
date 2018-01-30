9aa0aa2c17be0a2887ca4cf58aa381f074560654
Timo Kirkkala <timo.kirkkala@wunder.io>
2018-01-30 13:52:00 +0200

    FSA-724: Add targeting class for autosubmit (allow only from inside the block)

--- drupal/web/modules/custom/fsa_page_feedback/js/page-feedback.js
+++ drupal/web/modules/custom/fsa_page_feedback/js/page-feedback.js
@@ -2,7 +2,7 @@
   'use strict';
   Drupal.behaviors.fsaPageFeedback = {
     attach: function (context, settings) {
-      $('#edit-is-useful-yes').once().click(function(e) {
+      $('.form-autosubmit-on-yes #edit-is-useful-yes').once().click(function(e) {
         var form = $(this).closest('form');
         form.find(':submit').trigger('click');
         form.fadeOut('fast');
