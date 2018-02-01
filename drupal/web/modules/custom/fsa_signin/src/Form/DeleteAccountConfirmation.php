2b11dba924b905cb70e68848059ae55312f283b4
24ma13wg <24ma13wg@gmail.com>
2018-02-01 13:35:28 +0000

    FSA-795: Remove extra full stop

--- drupal/web/modules/custom/fsa_signin/src/Form/DeleteAccountConfirmation.php
+++ drupal/web/modules/custom/fsa_signin/src/Form/DeleteAccountConfirmation.php
@@ -50,7 +50,7 @@ class DeleteAccountConfirmation extends ConfirmFormBase {
     $email = $user->getEmail();
     $message = '<h1>' . $this->t('Confirm removal') . '</h1>';
     $message .= '<p>' . $this->t('You are about to remove subscription with email <strong>@email</strong>.', ['@email' => $email]) . '</p>';
-    $message .= '<p>' . $this->t('This will cancel all your subscriptions and permanently remove your personal details..') . '</p>';
+    $message .= '<p>' . $this->t('This will cancel all your subscriptions and permanently remove your personal details.') . '</p>';
     $message .= '<p>' . $privacy_link . '</p>';
     return $message;
   }
