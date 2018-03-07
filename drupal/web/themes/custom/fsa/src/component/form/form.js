44d5fbdaeb0d100b48523a999e83fb3110975059
Joshua Scott <jscott313@gmail.com>
2018-03-07 15:20:24 +0200

    FSA-929: Profile manager now shows error input element

---
+++ drupal/web/themes/custom/fsa/src/component/form/form.js
@@ -0,0 +1,17 @@
+import state from '../../helper/toggleHelpers'
+
+function form () {
+  // If there's an error, open the content where the error is
+  const profileManager = document.querySelector('#profile-manager')
+  const problemElement = profileManager.querySelector('input.error')
+
+  if (!profileManager || !problemElement) return
+
+  const toggleContent = problemElement.closest('.toggle-content')
+  const toggleButton = toggleContent.previousElementSibling
+
+  state.on({ element: toggleContent, type: 'content' }, 'is-open')
+  state.on({ element: toggleButton, type: 'button' }, 'is-open')
+}
+
+module.exports = form
