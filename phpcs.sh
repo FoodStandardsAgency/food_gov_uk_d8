56a28fe67f6be2e66606338193bc984ddd3529d6
Johan Gant <johan.gant@wunder.io>
2018-06-25 16:47:16 +0100

    Specify where the Drupal portion of the project lives and allow it to be passed as a parameter

--- phpcs.sh
+++ phpcs.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
+DRUPAL_DEPLOY_PATH=$1
 # We only care about our custom code folder and custom theme folder.
-PHPCS_CHECK_DIR=$1
+PHPCS_CHECK_DIR=$2
 # Dependencies are added with composer. Shouldn't be using a global install even if available.
-PHPCS_PATH="drupal/vendor/bin/phpcs"
-PHPCBF_PATH="drupal/vendor/bin/phpcbf"
+PHPCS_PATH="${DRUPAL_DEPLOY_PATH}/vendor/bin/phpcs"
+PHPCBF_PATH="${DRUPAL_DEPLOY_PATH}/vendor/bin/phpcbf"
 # Define extensions we're interested in checking.
 PHPCS_EXTENSIONS="php,inc,module,theme"
 # Exclude some fussier/less valuable sniffs.
@@ -13,9 +14,9 @@ DRUPAL_EXCLUDED_SNIFFS=(
     Drupal.Commenting.ClassComment
 )
 # Ignore some npm or non-PHP related FE toolchain directories.
-IGNORE="drupal/web/themes/custom/fsa/dist"
-IGNORE="$IGNORE,drupal/web/themes/custom/fsa/src"
-IGNORE="$IGNORE,drupal/web/themes/custom/fsa/node_modules"
+IGNORE="${DRUPAL_DEPLOY_PATH}/web/themes/custom/fsa/dist"
+IGNORE="$IGNORE,${DRUPAL_DEPLOY_PATH}/web/themes/custom/fsa/src"
+IGNORE="$IGNORE,${DRUPAL_DEPLOY_PATH}/web/themes/custom/fsa/node_modules"
 
 echo "Running coding standard checks in ${PHPCS_CHECK_DIR}"
 
