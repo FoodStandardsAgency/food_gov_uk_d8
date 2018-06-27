a5cec86b69e5bced5a7281860ec326216fcfe70f
Timo Kirkkala <timo.kirkkala@wunder.io>
2018-06-27 12:49:54 +0300

    Use DI on t() call

--- drupal/web/modules/custom/fsa_content_reminder/src/Form/FsaContentReminderSettings.php
+++ drupal/web/modules/custom/fsa_content_reminder/src/Form/FsaContentReminderSettings.php
@@ -34,7 +34,7 @@ class FsaContentReminderSettings extends ConfigFormBase {
 
     $form['info'] = [
       '#markup' => '<p>' . $this->t('Content reminders are sent to the email below. Reminders are based on the "Content reminder" field date value on node edit form.') . '<br />' .
-      t('Reminders are sent only if the node is published at the time of reminder dispatching.') . '</p>' .
+      $this->t('Reminders are sent only if the node is published at the time of reminder dispatching.') . '</p>' .
       '<p>' . $this->t('<a href="@url">List of pages pending content review</a>', ['@url' => '/admin/content/content-reminders']) . '</p>',
     ];
     $form['email'] = [
