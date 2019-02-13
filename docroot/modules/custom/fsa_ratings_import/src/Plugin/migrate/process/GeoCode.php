ed8a075e1e3946a81ff0976ce49d20a93cea92fb
Johan Gant <johan@wearesort.co.uk>
2019-02-13 15:34:55 +0000

    Improve migrate import geocoding
    Not all establishments provide geocoded locations. So we use a process plugin to try and fill the values by geocoding address or postcode field values

---
+++ docroot/modules/custom/fsa_ratings_import/src/Plugin/migrate/process/GeoCode.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Drupal\fsa_ratings_import\Plugin\migrate\process;
+
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\Row;
+
+/**
+ * Get lat/lon coords from AddressLine1 or PostCode values.
+ *
+ * @MigrateProcessPlugin(
+ *   id = "geocode",
+ * )
+ */
+class GeoCode extends ProcessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+
+    if (!empty($value)) {
+      return $value;
+    }
+
+    // Ask Google Geocode API for lat/lon values from the postcode value, if we have it.
+    $postcode = $row->getSourceProperty('PostCode');
+    $address1 = $row->getSourceProperty('AddressLine1');
+
+    $geocode_wkt;
+    // A lot of mobile traders put all address data into AddressLine1, try that.
+    if (!empty($address1)) {
+      $geocode_wkt = $address1;
+    }
+    // Prioritise postcode, if we have it.
+    if (!empty($postcode)) {
+      $geocode_wkt = $postcode;
+    }
+
+    $geocoder_id = 'google_geocoding_api';
+    $geocode_result = \Drupal::service('plugin.manager.geolocation.geocoder')
+      ->getGeoCoder($geocoder_id)
+      ->geocode($geocode_wkt);
+
+    if (!empty($geocode_result['location'])) {
+      $row->setDestinationProperty('field_geocode', $geocode_result['location']);
+    }
+  }
+
+}
