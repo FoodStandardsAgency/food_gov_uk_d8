96bd773ffbf177ed17b66533cc9139f140f55a20
John Ennew <johne@deeson.co.uk>
2019-03-10 22:04:42 +0000

    FSA-58: Changed the browser JS logic for printing the page to print both when the existing print page button is clicked and if the print guide link has been clicked. This opens the page in a new frame and adds the print=1 to the url

--- docroot/modules/custom/fsa_custom/js/page-print.js
+++ docroot/modules/custom/fsa_custom/js/page-print.js
@@ -2,10 +2,17 @@
   'use strict';
   Drupal.behaviors.fsaPagePrint = {
     attach: function (context, settings) {
-      $('.page-print-trigger').once().click(function(e) {
+      $('button.page-print-trigger').once().click(function(e) {
         e.preventDefault();
         window.print();
         return false;
+      });
+
+      $( document ).ready(function() {
+        if (window.location.href.indexOf('?print=1') !== -1 && $('body').hasClass('theme--multipage_guide')) {
+          // On multi page guide pages, trigger the print function if its requested in the url.
+          window.print();
+        }
       });
     }
   };
