dcc7f738b6ae2939b6fb77d4d0a258d80337e3c0
Modesto Caballero <modesto.caballero@gmail.com>
2019-07-10 11:55:40 +0100

    FSA-15: Adding the text (Opens in a new window) to links with target _blank.

---
+++ docroot/modules/custom/fsa_custom/js/fsa-external.js
@@ -0,0 +1,11 @@
+(function ($) {
+  'use strict';
+  Drupal.behaviors.fsaExternal = {
+    attach: function (context, settings) {
+      // Excluding social media icons.
+      $(":not('.social-media') a[target=_blank]:not(.social-media__link)").each(function () {
+        $(this).text($(this).text() + ' ' + Drupal.t('(Opens in a new window)');
+      });
+    }
+  };
+}(jQuery));
