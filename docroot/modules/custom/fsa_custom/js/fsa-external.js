386fb6934ced2df87e9efaa121d3a6354bb892fc
Modesto Caballero <modesto.caballero@gmail.com>
2019-07-11 17:24:06 +0100

    FSA-133: External links (Opens on a new window) message are hidden for sighted visitors.

--- docroot/modules/custom/fsa_custom/js/fsa-external.js
+++ docroot/modules/custom/fsa_custom/js/fsa-external.js
@@ -3,8 +3,9 @@
   Drupal.behaviors.fsaExternal = {
     attach: function (context, settings) {
       // Excluding social media icons.
-      $(":not('.social-media') a[target=_blank]:not(.social-media__link)").each(function () {
-        $(this).text($(this).text() + ' ' + Drupal.t('(Opens in a new window)'));
+      $("a[target=_blank]:not(.social-media__link)").each(function () {
+        $(this).html($(this).text() + ' <span class="visuallyhidden">'
+          + Drupal.t('(Opens in a new window)')) + '</span>';
       });
     }
   };
