dd01630256a2536981b10d0c1c10a39563f21b06
Modesto Caballero <modesto.caballero@gmail.com>
2019-07-26 12:53:58 +0100

    FSA-7: Focus into addtoany "More" popup, added timeout.

--- docroot/modules/custom/fsa_custom/js/fsa-addtoany-accesibility.js
+++ docroot/modules/custom/fsa_custom/js/fsa-addtoany-accesibility.js
@@ -3,7 +3,7 @@
   Drupal.behaviors.fsaAddtoanyAccesibility = {
     attach: function (context, settings) {
       // Changing focus to the addtoany popup after clicking on "More..."
-      $('.a2a_menu_show_more_less').on('click', function(){
+      $(document).on('click', '.a2a_menu_show_more_less',function() {
         $('#a2apage_full .a2a_i:first-child').focus();
       });
     }
