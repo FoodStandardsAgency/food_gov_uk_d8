e1f2cdc63e4e284002a10028527e3e01b3e69d12
Peter Berryman <peterb@deeson.co.uk>
2019-07-30 11:55:13 +0100

    [FSA-7] fix

--- docroot/modules/custom/fsa_custom/js/fsa-addtoany-accesibility.js
+++ docroot/modules/custom/fsa_custom/js/fsa-addtoany-accesibility.js
@@ -1,10 +1,25 @@
 (function ($) {
   'use strict';
+  var waitForEl = function(selector, callback) {
+    if (jQuery(selector).length) {
+      callback();
+    } else {
+      setTimeout(function() {
+        waitForEl(selector, callback);
+      }, 100);
+    }
+  };
+
   Drupal.behaviors.fsaAddtoanyAccesibility = {
     attach: function (context, settings) {
       // Changing focus to the addtoany popup after clicking on "More..."
-      $(document).on('click', '.a2a_menu_show_more_less',function() {
-        $('#a2apage_full .a2a_i:first-child').focus();
+      // We can't seem to use a live event here.
+      waitForEl('#a2apage_show_more_less', function () {
+        $('#a2apage_show_more_less').click(function (e) {
+          setTimeout(function () {
+            $('#a2apage_full .a2a_i:first-child').focus();
+          }, 100);
+        });
       });
     }
   };
