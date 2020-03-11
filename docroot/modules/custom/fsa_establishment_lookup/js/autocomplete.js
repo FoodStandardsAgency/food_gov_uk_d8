7db8756bce43cba6dbac399aaf2f3963d0c75aac
AlexKDeeson <alexander.kirsten@gmail.com>
2020-03-11 12:39:43 +0000

    [FSA-191] - Added logic for adding / removing marker.

--- docroot/modules/custom/fsa_establishment_lookup/js/autocomplete.js
+++ docroot/modules/custom/fsa_establishment_lookup/js/autocomplete.js
@@ -2,101 +2,65 @@
 
     'use strict';
 
-    var map;
+    var map,
+        markersArray = [],
+        autocomplete,
+        element_id;
 
     Drupal.behaviors.mybehavior = {
         attach: function (context, settings) {
 
-            function init_places_autocomplete() {
-                var element_id = drupalSettings.fsa_establishment_lookup.googleplaces.element_id;
-                var input = document.getElementById(element_id);
-                var options = {
-                    componentRestrictions: {
-                        country: "gb"
-                    }
-                };
-                var autocomplete = new google.maps.places.Autocomplete(input, options);
-                autocomplete.setTypes(['establishment']);
+            var _this = this;
 
-                // Get the postal code from selected google places entry.
-                google.maps.event.addListener(autocomplete, 'place_changed', function() {
-                    var postal_code = '',
-                        address = '',
-                        postal_town = '';
-                    var place = autocomplete.getPlace(),
-                        establishment = place.name;
+            // Get autocomplete input ID from settings.
+            element_id = drupalSettings.fsa_establishment_lookup.googleplaces.element_id;
+            var input = document.getElementById(element_id);
 
-                    console.log(place);
-                    //google.maps.event.trigger(place, 'click')
-                    var geocoder = new google.maps.Geocoder;
-                    var infowindow = new google.maps.InfoWindow;
-                    geocoder.geocode({'placeId': place.place_id}, function(results, status) {
-                        if (status === 'OK') {
-                            if (results[0]) {
-                                $('#map').show();
-                                map.setZoom(11);
-                                map.setCenter(results[0].geometry.location);
-                                var marker = new google.maps.Marker({
-                                    map: map,
-                                    position: results[0].geometry.location
-                                });
-                                infowindow.setContent(results[0].formatted_address);
-                                infowindow.open(map, marker);
-                            }
-                        }
-                    });
+            // Init Google geolocation autocomplete input.
+            autocomplete = new google.maps.places.Autocomplete(input, {
+                componentRestrictions: {
+                    country: 'gb'
+                }
+            });
+            autocomplete.setTypes(['establishment']);
 
-                    for (var i = 0; i < place.address_components.length; i++) {
-                        for (var j = 0; j < place.address_components[i].types.length; j++) {
-                            switch (place.address_components[i].types[j]) {
-                                case  'postal_code':
-                                    postal_code = place.address_components[i].long_name;
-                                    break;
-                                case 'route':
-                                    address = place.address_components[i].long_name;
-                                    break;
-                                case 'postal_town':
-                                    postal_town = place.address_components[i].long_name;
-                                    break;
-                            }
-                        }
-                    }
-
-                    // Replace field values with more details of the establishment.
-                    $('#'+element_id).val(establishment+', ' +address+', ' +postal_code+', '+postal_town);
-
-                    // Set postal code to respective field.
-                    // @todo: instead of creating the hidden field via configs extend the custom webform plugin.
-                    $('input[data-drupal-selector="edit-fsa-establishment-postal-code"]').val(postal_code)
-
-                });
-            }
-            google.maps.event.addDomListener(window, 'load', init_places_autocomplete);
+            // Triggers behaviour when autocomplete used.
+            google.maps.event.addListener(autocomplete, 'place_changed', _this.handleAutocompleteChange);
 
             // Triggers Google map on CTA click.
-            var _this = this;
-            $('a#map-trigger').click(function(e) {
+            $('a#map-trigger').click(function (e) {
                 e.preventDefault();
 
-                // Init map.
-                map = _this.initMap();
+                // Check if map hasn't already been initialised.
+                if (typeof map === 'undefined') {
+                    _this.initMap();
+                }
 
-                // Adds Google maps event listener to place marker.
-                map.addListener('click', _this.placeMarker);
+                // Default to users location with their permission.
+                _this.detectUserLocation();
+
+                // Show map.
+                $('#map').show();
             });
         },
-        initMap: function() {
+        // Initialises google maps object when map shown or autocomplete used.
+        initMap: function () {
+            var _this = Drupal.behaviors.mybehavior;
+
             // Show map and set default location to London.
-            $('#map').show();
-            var map = new google.maps.Map(document.getElementById('map'), {
+            map =  new google.maps.Map(document.getElementById('map'), {
                 center: {lat: 51.5074, lng: 0.1278},
                 mapTypeControl: false,
                 zoom: 15
             });
 
-            // If user allows address access then set their position on map.
+            // Adds Google maps event listener to place marker.
+            map.addListener('click', _this.handlePlaceClick);
+        },
+        // Detect user location.
+        detectUserLocation: function () {
             if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(function(position) {
+                navigator.geolocation.getCurrentPosition(function (position) {
                     var pos = {
                         lat: position.coords.latitude,
                         lng: position.coords.longitude
@@ -105,10 +69,56 @@
                     map.setZoom(15);
                 });
             }
-
-            return map;
         },
-        placeMarker: function(event) {
+        handleAutocompleteChange: function () {
+            var _this = Drupal.behaviors.mybehavior;
+
+            // Init Geocoder and InfoWindow objects.
+            var geocoder = new google.maps.Geocoder;
+            var infowindow = new google.maps.InfoWindow;
+
+            var place = autocomplete.getPlace();
+            geocoder.geocode({'placeId': place.place_id}, function (results, status) {
+                if (status === 'OK') {
+                    if (results[0]) {
+                        // Check if map hasn't already been initialised.
+                        if (typeof map === 'undefined') {
+                            _this.initMap();
+                        }
+                        
+                        // Remove previous markers.
+                        _this.removeMapMarkers();
+
+                        // Show map and create markers.
+                        $('#map').show();
+                        map.setZoom(17);
+                        map.setCenter(results[0].geometry.location);
+                        var marker = new google.maps.Marker({
+                            map: map,
+                            position: results[0].geometry.location
+                        });
+                        markersArray.push(marker);
+
+                        // Set info window content.
+                        var infoContent = _this.formatInfoWindowAddress(place);
+                        infowindow.setContent(infoContent);
+                        infowindow.open(map, marker);
+                    }
+                }
+            });
+
+            // Set address input.
+            var nameAddress = place.name + ', ' + place.formatted_address;
+            $('#'+element_id).val(nameAddress);
+
+            // Set hidden field with postcode.
+            // @todo: instead of creating the hidden field via configs extend the custom webform plugin.
+            var postCode = _this.getPlacePostcode(place);
+            $('input[data-drupal-selector="edit-fsa-establishment-postal-code"]').val(postCode);
+
+        },
+        // Event handler for clicking Google place within map.
+        handlePlaceClick: function (event) {
             var _this = Drupal.behaviors.mybehavior;
 
             // Only place markers for actual Google places.
@@ -117,28 +127,35 @@
             }
 
             // Look up address of Google place and set address inputs.
-            _this.getPlaceAddress(event.placeId).then(function(place) {
-                var element_id = drupalSettings.fsa_establishment_lookup.googleplaces.element_id;
+            _this.getPlaceAddress(event.placeId).then(function (place) {
+                // Removes previous markers.
+                _this.removeMapMarkers();
+
+                // Add new marker.
+                var marker = new google.maps.Marker({
+                    map: map,
+                    position: event.latLng
+                });
+                markersArray.push(marker);
 
                 // Set address input.
                 var nameAddress = place.name + ', ' + place.formatted_address;
+                element_id = drupalSettings.fsa_establishment_lookup.googleplaces.element_id;
                 $('#'+element_id).val(nameAddress);
 
-                // Loop through address components and extract postcode.
-                $(place.address_components).each(function(index, component) {
-                    if (component.types[0] === 'postal_code') {
-                        // Set hidden field with postcode value.
-                        $('input[data-drupal-selector="edit-fsa-establishment-postal-code"]').val(component.long_name)
-                    }
-                });
+                // Set hidden field with postcode.
+                var postCode = _this.getPlacePostcode(place);
+                $('input[data-drupal-selector="edit-fsa-establishment-postal-code"]').val(postCode);
+
             })
             .catch(function (error) {
                 console.log(error.message);
             });
         },
+        // Returns a promise while querying Google Places service.
         getPlaceAddress: function (placeId) {
             return new Promise(
-                function(resolve, reject) {
+                function (resolve, reject) {
                     // Google places API request object.
                     var request = {
                         placeId: placeId,
@@ -147,7 +164,7 @@
 
                     // Query Google places API.
                     var service = new google.maps.places.PlacesService(map);
-                    service.getDetails(request, function(place, status) {
+                    service.getDetails(request, function (place, status) {
                         if (status === google.maps.places.PlacesServiceStatus.OK) {
                             resolve(place);
                         }
@@ -157,6 +174,58 @@
                     });
                 }
             );
+        },
+        // Removes all markers from Google map.
+        removeMapMarkers: function () {
+            if (markersArray) {
+                for (var i in markersArray) {
+                    markersArray[i].setMap(null);
+                }
+                markersArray.length = 0;
+            }
+        },
+        // Formats marker address to match the format default markers use.
+        formatInfoWindowAddress: function (place) {
+            var _this = Drupal.behaviors.mybehavior,
+                formattedAddress,
+                numberStreet = '',
+                boroughCity = '',
+                postCode ='';
+
+            // Loop through address components and format parts.
+            $(place.address_components).each(function (index, component) {
+                switch (component.types[0]) {
+                    case 'street_number':
+                        numberStreet += component.long_name;
+                        break;
+                    case 'route':
+                        numberStreet += ' ' + component.long_name;
+                        break;
+                    case 'neighborhood':
+                        boroughCity += component.long_name;
+                        break;
+                    case 'postal_town':
+                        boroughCity += ', ' + component.long_name;
+                        break;
+                    case 'postal_code':
+                        postCode = component.long_name;
+                        break;
+                }
+            });
+            formattedAddress = '<strong>' + place.name + '</strong><br>';
+            formattedAddress += numberStreet + '<br>';
+            formattedAddress += boroughCity + '<br>';
+            formattedAddress += postCode;
+
+            return formattedAddress;
+        },
+        // Extracts postcode from Google place object.
+        getPlacePostcode: function (place) {
+            $(place.address_components).each(function (index, component) {
+                if (component.types[0] === 'postal_code') {
+                    return component.long_name;
+                }
+            });
         }
     };
 
