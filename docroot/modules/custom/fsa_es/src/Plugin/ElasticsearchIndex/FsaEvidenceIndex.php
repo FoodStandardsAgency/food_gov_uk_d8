245d98d0416d71cbe58d83f60100dfcade16aceb
AlexKDeeson <alexander.kirsten@gmail.com>
2020-10-28 10:17:00 +0000

    [FSA-298]
    - Implemented Normalizer service for Evidence bundles (Elasticsearch expected format)
    - Created FsaEvidenceIndex

---
+++ docroot/modules/custom/fsa_es/src/Plugin/ElasticsearchIndex/FsaEvidenceIndex.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Drupal\fsa_es\Plugin\ElasticsearchIndex;
+
+/**
+ * @ElasticsearchIndex(
+ *   id = "evidence_index",
+ *   label = @Translation("FSA Evidence Index"),
+ *   indexName = "evidence-{langcode}",
+ *   entityType = "node",
+ *   bundle = "evidence",
+ *   typeName = "evidence",
+ * )
+ */
+class FsaEvidenceIndex extends FsaIndexBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setup() {
+    // Create one index per language, so that we can have different analyzers.
+    foreach ($this->languageManager->getLanguages() as $langcode => $language) {
+      $index_name = 'page-' . $langcode;
+
+      if (!$this->client->indices()->exists(['index' => $index_name])) {
+        $this->client->indices()->create([
+          'index' => $index_name,
+          'body' => [
+            'number_of_shards' => 1,
+            'number_of_replicas' => 0,
+          ] + $this->getFiltersAndAnalyzers($langcode),
+        ]);
+
+        // Get language name which is also a text analyzer name.
+        $text_analyzer = $this->getLanguageName($langcode);
+
+        $mapping = [
+          'index' => $index_name,
+          'type' => 'page',
+          'body' => [
+            'properties' => [
+              'id' => [
+                'type' => 'integer',
+              ],
+              'entity_type' => [
+                'type' => 'keyword',
+              ],
+              'langcode' => [
+                'type' => 'keyword',
+              ],
+              'name' => [
+                'type' => 'text',
+                'analyzer' => $text_analyzer,
+              ],
+              'intro' => [
+                'type' => 'text',
+                'analyzer' => $text_analyzer,
+              ],
+              'body' => [
+                'type' => 'text',
+                'analyzer' => $text_analyzer,
+              ],
+              'type' => [
+                'properties' => [
+                  'id' => ['type' => 'keyword'],
+                  'label' => [
+                    'type' => 'text',
+                    'index' => FALSE,
+                    'fields' => [
+                      'keyword' => [
+                        'type' => 'keyword',
+                      ],
+                    ],
+                  ],
+                ],
+              ],
+              'topic' => [
+                'properties' => [
+                  'id' => ['type' => 'keyword'],
+                  'depth' => ['type' => 'integer'],
+                  'label' => [
+                    'type' => 'text',
+                    'index' => FALSE,
+                    'fields' => [
+                      'keyword' => [
+                        'type' => 'keyword',
+                      ],
+                    ],
+                  ],
+                ],
+              ],
+              'nation' => [
+                'properties' => [
+                  'id' => ['type' => 'keyword'],
+                  'label' => [
+                    'type' => 'text',
+                    'index' => FALSE,
+                    'fields' => [
+                      'keyword' => [
+                        'type' => 'keyword',
+                      ],
+                    ],
+                  ],
+                ],
+              ],
+              'created' => [
+                'type' => 'date',
+              ],
+              'updated' => [
+                'type' => 'date',
+              ],
+            ],
+          ],
+        ];
+
+        $this->client->indices()->putMapping($mapping);
+      }
+    }
+  }
+
+}
---
+++ docroot/modules/custom/fsa_es/src/Plugin/Normalizer/FsaEvidenceNormalizer.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Drupal\fsa_es\Plugin\Normalizer;
+
+use Drupal\Core\Datetime\DateFormatterInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+
+/**
+ * Normalizes Drupal entities into an array structure good for ES.
+ */
+class FsaEvidenceNormalizer extends NormalizerBase {
+
+  use StringTranslationTrait;
+
+  /**
+   * @var array
+   */
+  protected $taxonomyTreeCache = [];
+
+  /**
+   * The interface or class that this Normalizer supports.
+   *
+   * @var array
+   */
+  protected $supportedInterfaceOrClass = ['\Drupal\node\Entity\Node'];
+
+  /**
+   * Supported formats.
+   *
+   * @var array
+   */
+  protected $format = ['elasticsearch_helper'];
+
+  /**
+   * @var \Drupal\Core\Datetime\DateFormatterInterface
+   */
+  protected $dateFormatter;
+
+  /**
+   * FsaPageNormalizer constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   Entity manager interface.
+   * @param \Drupal\Core\Datetime\DateFormatterInterface $date_formatter
+   *   Date formatter interface.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, DateFormatterInterface $date_formatter) {
+    parent::__construct($entity_type_manager);
+    $this->dateFormatter = $date_formatter;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function supportsNormalization($data, $format = NULL) {
+    return is_a($data, '\Drupal\node\Entity\Node') && $data->bundle() == 'page';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function normalize($object, $format = NULL, array $context = []) {
+    $parent_data = parent::normalize($object, $format, $context);
+
+    // Get dates.
+    $entity_dates = [];
+    foreach (['created', 'changed'] as $date_field) {
+      $entity_dates[$date_field] = $this->dateFormatter->format($object->get($date_field)->value, 'custom', DATETIME_DATETIME_STORAGE_FORMAT, DATETIME_STORAGE_TIMEZONE);
+    }
+
+    $data = [
+      'name' => $this->getTranslatedLabel($object),
+      'intro' => $this->prepareTextualField($object->get('field_intro')->getString()),
+      'body' => $this->prepareTextualField($object->get('body')->getString()),
+      'nation' => array_map(function ($item) {
+        return [
+          'id' => $item->id(),
+          'label' => $this->getTranslatedLabel($item),
+        ];
+      }, $object->get('field_nation')->referencedEntities()),
+      'topic' => array_map(function ($item) {
+        return [
+          'id' => $item->id(),
+          'label' => $this->getTranslatedLabel($item),
+        ];
+      }, $object->get('field_topic')->referencedEntities()),
+      'type' => array_map(function ($item) {
+        return [
+          'id' => $item->id(),
+          'label' => $this->getTranslatedLabel($item),
+        ];
+      }, $object->get('field_evidence_type')->referencedEntities()),
+      'created' => $entity_dates['created'],
+      'updated' => $entity_dates['changed'],
+    ] + $parent_data;
+
+    return $data;
+  }
+
+}
