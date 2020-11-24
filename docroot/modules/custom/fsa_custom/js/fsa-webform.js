8eb437b202badfd63487040c70aafb179b4ed072
Carole Ancia <carolea@deeson.co.uk>
2020-11-24 17:13:24 +0000

    [FSA-301] fixing anchor and fields css for email fields

--- docroot/modules/custom/fsa_custom/js/fsa-webform.js
+++ docroot/modules/custom/fsa_custom/js/fsa-webform.js
@@ -5,6 +5,11 @@
     attach: function (context, settings) {
       // removing aria-describedby attribute on confirm email field
       $('#edit-reg-products-emails-confirm-mail-2').removeAttr("aria-describedby");
+
+      //renaming the anchor for the email when there's an error to the proper id
+      if ($('.error-summary a[href="#edit-reg-products-emails-confirm"]').length != 0) {
+        $('.error-summary a[href="#edit-reg-products-emails-confirm"]').attr('href', '#edit-reg-products-emails-confirm--wrapper');
+      }
     }
   };
 }(jQuery));
