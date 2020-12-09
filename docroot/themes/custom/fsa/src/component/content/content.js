668b52a8799717cab49343c57787b6b90f1b46ca
Peter Berryman <peterb@deeson.co.uk>
2020-12-09 10:40:00 +0000

    Revert "[FSA-41] - Welsh exceptions"
    This reverts commit 4398e79bde7daf81382953071ed27c1461a07bcf.

--- docroot/themes/custom/fsa/src/component/content/content.js
+++ docroot/themes/custom/fsa/src/component/content/content.js
@@ -20,13 +20,13 @@ function addHeading () {
       heading.innerHTML = hasDrupal ? Drupal.t(`England`) : 'England';
     } else if (element.classList.contains('js-england-wales')) {
       heading.classList.add(`heading--small`);
-      heading.innerHTML = hasDrupal ? Drupal.t(`England and Wales`) : 'England and Wales';
+      heading.innerHTML = hasDrupal ? Drupal.t(`England and wales`) : 'England and Wales';
     } else if (element.classList.contains('js-england-northern-ireland')) {
       heading.classList.add(`heading--small`);
       heading.innerHTML = hasDrupal ? Drupal.t(`England and Northern Ireland`) : 'England and Northern Ireland';
     } else if (element.classList.contains('js-northern-ireland-wales')) {
       heading.classList.add(`heading--small`);
-      heading.innerHTML = hasDrupal ? Drupal.t('Northern Ireland and Wales') : 'Northern Ireland and wales';
+      heading.innerHTML = hasDrupal ? Drupal.t('Northern Ireland and wales') : 'Northern Ireland and wales';
     } else if (element.classList.contains('js-wales')) {
       heading.classList.add(`heading--small`);
       heading.innerHTML = hasDrupal ? Drupal.t(`Wales`) : 'Wales';
